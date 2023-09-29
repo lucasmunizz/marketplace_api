609a0f2c752cc33929fa9a81ba54284642eb5100
Lucas Muniz <lucaslima1048@gmail.com>
2023-09-29 19:48:22 -0300

    add namespace routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@
 Rails.application.routes.draw do
   mount Rswag::Api::Engine => '/api-docs'
   mount Rswag::Ui::Engine => '/api-docs'
+
+
+  namespace :api, defaults: {format: :json},
+                             constraints: {subdomain: 'api'}, path: '/' do
+
+  end
 end
